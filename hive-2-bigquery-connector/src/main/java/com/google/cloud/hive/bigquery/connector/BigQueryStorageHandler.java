2c71e8f84e632845dbadb787a4e91fe55eb62b6a
Julien Phalip <jphalip@gmail.com>
2023-08-11 22:52:54 -0700

    Update POMs

--- hive-2-bigquery-connector/src/main/java/com/google/cloud/hive/bigquery/connector/BigQueryStorageHandler.java
+++ hive-2-bigquery-connector/src/main/java/com/google/cloud/hive/bigquery/connector/BigQueryStorageHandler.java
@@ -1,9 +1,6 @@
 package com.google.cloud.hive.bigquery.connector;
 
-import java.util.Map;
-
 import org.apache.hadoop.hive.metastore.HiveMetaHook;
-import org.apache.hadoop.hive.ql.plan.TableDesc;
 
 public class BigQueryStorageHandler extends BigQueryStorageHandlerBase {
 
